b96165f820a7002906bab77a1a9fe8fe6a4bddc2
raphaelcarlosr.dev <raphaelcarlosr@gmail.com>
2025-02-24 22:43:26 +0100

    Initial commit with default files

--- bart.sh
+++ bart.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # shellcheck disable=all
 # Basic Application Repository Template
+
 bart:log() {
     local status=$?
     local args=()
@@ -24,7 +25,16 @@ bart:log() {
     unset args status funcName
     return 0
 }
-bart:strings:trim(){
+bart:log:pipe() {
+    [[ -t 0 ]] && return 0
+    local line
+    while IFS= read -r line || [ -n "$line" ]; do
+        bart:log "${@}" "$line"
+    done
+    unset line
+    return 0
+}
+bart:strings:trim() {
     local str="${1}"
     str="${str#"${str%%[![:space:]]*}"}"
     str="${str%"${str##*[![:space:]]}"}"
@@ -32,7 +42,7 @@ bart:strings:trim(){
     unset str
     return 0
 }
-bart:code:run(){
+bart:code:run() {
     local langName="${1}" && shift
     local code="${1}"
     {
@@ -45,7 +55,10 @@ bart:code:run(){
             eval "${code}"
             ;;
         "python")
-            command -v python3 >/dev/null 2>&1 || { bart:log "error" "Python compiler not found"; return 1; }
+            command -v python3 >/dev/null 2>&1 || {
+                bart:log "error" "Python compiler not found"
+                return 1
+            }
             # remove shebang if present
             python3 -c "$(echo "${code}" | sed '/^#!/d')"
             ;;
@@ -96,14 +109,29 @@ bart:markdown:read() {
         ["lines"]=0
         ["codes"]=0
     )
+    declare -A -g -I DOC=()
     ! test -f "${src["input"]}" && bart:log "error" "File not found: ${src["input"]}" && unset src && return 1
     src[lines]=0
-    local codeStartAt codeEndAt codeLang codeBlock trimedLine
+    local codeStartAt codeEndAt codeLang codeBlock trimedLine isVarBlock
     while IFS= read -r line || [ -n "$line" ]; do
         trimedLine=$(bart:strings:trim "$line")
         ((src[lines]++))
         # bart:log "trace" "Reading line ${src["lines"]}"
-        if [[ $trimedLine == '```'* && -z "${codeStartAt}" ]]; then
+        # read variables from --- block
+        if [[ $trimedLine == '---' && -n "${isVarBlock}" ]]; then
+            unset isVarBlock        
+        elif [[ $trimedLine == '---' && -z "${isVarBlock}" ]]; then
+            isVarBlock=1
+        elif [[ -n "${isVarBlock}" ]]; then
+            # read variable line
+            # bart:echo "$line" # 7>&1
+            IFS=':' read -r key value <<<"$trimedLine"
+            key=$(bart:strings:trim "$key")
+            value=$(bart:strings:trim "$value")
+            bart:log "trace" "Variable" "${key}" "=" "${value}"
+            DOC["${key}"]="${value}"
+            unset key value        
+        elif [[ $trimedLine == '```'* && -z "${codeStartAt}" ]]; then
             codeStartAt=${src["lines"]}
             codeEndAt=
             codeLang=$(echo "${line//\`/}" | awk '{print $1}')
@@ -117,10 +145,14 @@ bart:markdown:read() {
         elif [[ -n "${codeStartAt}" && -z "${codeEndAt}" ]]; then
             codeBlock+="$trimedLine"$'\n'
         elif [[ -n "${codeStartAt}" && -n "${codeEndAt}" ]]; then
+            # bart:echo -n '```'${codeLang}'' $'\n' "${codeBlock}" '```' $'\n'
             bart:log "info" "Running code block ${src["codes"]} in ${codeLang}" "${#codeBlock} characters"
-            # eval "$codeBlock" 2>&1
             bart:code:run "${codeLang}" "${codeBlock}" # >&7
-            [[ $? -ne 0 ]] && bart:log "warning" "Failed to run code block ${src["codes"]} in ${codeLang}"
+            if [[ $? -ne 0 ]]; then
+                bart:log "warning" "Failed to run code block ${src["codes"]} in ${codeLang}"
+            else
+                bart:log "info" "Executed code block ${src["codes"]} in ${codeLang}."
+            fi
             codeStartAt=""
             codeEndAt=""
             codeLang=""
@@ -130,25 +162,62 @@ bart:markdown:read() {
             bart:echo "$line" # 7>&1 >&7
         fi
     done <"${src["input"]}" 7>&1
-    unset src line
+    unset src line trimedLine codeStartAt codeEndAt codeLang codeBlock
     return 0
 }
-bart:licence:generate(){
+bart:licence:generate() {
     local licenceName="${1}"
-    local licenceFile="${2:-./LICENSE}"
-    local licenceText
+    [[ ! "${licenceName}" =~ (mit|lgpl-3.0|mpl-2.0|agpl-3.0|unlicense|apache-2.0|gpl-3.0) ]] && bart:log "error" "Invalid licence name: ${licenceName}" && return 1
+    bart:log "info" "Generating ${licenceName} licence"
     # https://choosealicense.com/
-    # https://chooser-beta.creativecommons.org/    
+    # https://chooser-beta.creativecommons.org/
+    wget -qO- "https://api.github.com/licenses/${licenceName}" >&7
+    unset licenceName
+    return 0
 }
 bart:echo() {
     echo "${@}" >&7 # 7>&1
+    return 0
+}
+bart:github:files() {
+    # create default github files
+    local files=(
+        "src/bart/.github/CODE_OF_CONDUCT.md"
+        "src/bart/.github/CONTRIBUTING.md"
+        "src/bart/.github/ISSUE_TEMPLATE.md"
+        "src/bart/.github/PULL_REQUEST_TEMPLATE.md"
+        "src/bart/.github/SECURITY.md"
+        "src/bart/.github/CODEOWNERS"
+    )
+    for file in "${files[@]}"; do
+        bart:log "info" "Creating ${file}"
+        test -f "${file}" && rm -f "${file}"
+        touch "${file}" && chmod 644 "${file}"
+        local fileDir=$(dirname "${file}")
+        [[ ! -d "${fileDir}" ]] && mkdir -p "${fileDir}"
+        unset fileDir        
+        if [[ "${file}" == *"CODE_OF_CONDUCT.md" ]]; then
+            echo "# Code of Conduct" >"${file}"
+        elif [[ "${file}" == *"CONTRIBUTING.md" ]]; then
+            echo "# Contributing" >"${file}"
+        elif [[ "${file}" == *"ISSUE_TEMPLATE.md" ]]; then
+            echo "# Issue Template" >"${file}"
+        elif [[ "${file}" == *"PULL_REQUEST_TEMPLATE.md" ]]; then
+            echo "# Pull Request Template" >"${file}"
+        elif [[ "${file}" == *"SECURITY.md" ]]; then
+            echo "# Security" >"${file}"
+        elif [[ "${file}" == *"CODEOWNERS" ]]; then
+            echo "* @ywteam" >"${file}"
+        fi
+    done
+    unset files file
     return 0
 }
 bart() {
     ! test -p /tmp/bart.sock && mkfifo /tmp/bart.sock
     exec 7<>/tmp/bart.sock
     bart:log "info" "Running bart.sh"
-    bart:markdown:read ./README.mds  > README.md
+    bart:markdown:read ./README.mds >README.md
     bart:log "info" "Finished processing README.mds"
     return 0
 }
